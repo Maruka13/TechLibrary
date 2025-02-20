dbbf6d01ce92e0c67c6f3a10cf02fbaf93e4912c
Maruka <emanuelle_carollina@hotmail.com>
2025-02-19 22:49:11 -0300

    users controller

---
+++ TechLibrary.api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechLibrary.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Create();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Create();
+        }
+    }
+}
