1f7114898b5b88890feff907bf08d907df833202
Maruka <emanuelle_carollina@hotmail.com>
2025-02-20 01:00:49 -0300

    user register

---
+++ TechLibrary.api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -0,0 +1,18 @@
+﻿using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+
+namespace TechLibrary.api.UseCases.Users.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestUserJson request)
+        {
+
+
+            return new ResponseRegisteredUserJson
+            {
+
+            };
+        }
+    }
+}
---
+++ TechLibrary.api/UseCases/Users/Register/RegisterUserValidator.cs
@@ -0,0 +1,16 @@
+﻿using TechLibrary.Communication.Requests;
+using FluentValidation;
+
+namespace TechLibrary.api.UseCases.Users.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestUserJson>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(request => request.Name).NotEmpty().WithMessage("Nome obrigatório.");
+            RuleFor(request => request.Email).EmailAddress().WithMessage("Email inválido.");
+            RuleFor(request => request.Password).NotEmpty().WithMessage("Senha obrigatória.");
+            RuleFor(request => request.Password.Length).GreaterThanOrEqualTo(6).WithMessage("Senha deve ter 6 ou mais caracteres.");
+        }
+    }
+}
