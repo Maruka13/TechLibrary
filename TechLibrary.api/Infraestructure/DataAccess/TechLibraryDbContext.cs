25172e7030ac2c0c207b373172a68f23362f452d
Maruka <emanuelle_carollina@hotmail.com>
2025-02-22 14:42:24 -0300

    pagination and filters

--- TechLibrary.api/Infraestructure/DataAccess/TechLibraryDbContext.cs
+++ TechLibrary.api/Infraestructure/DataAccess/TechLibraryDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TechLibrary.api.Domain.Entities;
+using TechLibrary.Api.Domain.Entities;
 
 namespace TechLibrary.api.Infraestructure.DataAccess
 {
     public class TechLibraryDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Checkout> Checkouts { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=C:\\Users\\emanu\\OneDrive\\Área de Trabalho\\TechLibraryDb.db");
